c987bfec24295031c14a1f397bfaff1260c8b8e3
strokk <35735075+strokk@users.noreply.github.com>
2018-02-08 19:06:05 +0000

    Create fib.py

---
+++ fib.py
@@ -0,0 +1,19 @@
+# Guilherme Paes
+# A program that displays Fibonacci numbers.
+
+def fib(n):
+  """This function returns the nth Fibonacci number."""
+  i = 0
+  j = 1
+  n = n - 1
+
+  while n >= 0:
+    i, j = j, i + j
+    n = n - 1
+  
+  return i
+
+# Test the function with the following value.
+x = 12
+ans = fib(x)
+print("Fibonacci number", x, "is", ans)
