a946fb9b23f5f8506a2174427d05000e11ea5f83
strokk <35735075+strokk@users.noreply.github.com>
2018-03-21 23:10:24 +0000

    Update fib.py
    Added some comments and also included Exercise 2 in the same file

--- fib.py
+++ fib.py
@@ -1,5 +1,8 @@
-# Guilherme Paes
+# Guilherme Paes Feb 2018
+
+# GMIT Data Analytics Exercise 1:
 # A program that displays Fibonacci numbers (Adapted from Ian McLoughlin)
+
 
 def fib(n):
   """This function returns the nth Fibonacci number."""
@@ -19,4 +22,29 @@ ans = fib(x)
 print("Fibonacci number", x, "is", ans)
 
 
-#My name is Guilherme, so the first and last letter of my name (G + E = 7 + 5) give the number 12. The 12th Fibonacci number is 144.
+# My name is Guilherme, so the first and last letter of my name (G + E = 7 + 5) give the number 12. The 12th Fibonacci number is 144.
+
+
+# GMIT Data Analytics Exercise 2:
+"""The ord() method returns an integer representing Unicode code point for the given Unicode character.
+In other words, it gives the ASCII code of the character.
+Example: ord(‘d’) is 100"""
+
+name = "Paes"
+first = name[0]
+last = name[-1]
+firstno = ord(first)
+lastno = ord(last)
+x = firstno + lastno
+
+ans = fib(x)
+print("My surname is", name)
+print("The first letter", first, "is number", firstno)
+print("The last letter", last, "is number", lastno)
+print("Fibonacci number", x, "is", ans)
+
+
+"""My surname is Paes
+The first letter P is number 80
+The last letter s is number 115
+Fibonacci number 195 is 25299086886458645685589389182743678652930"""
