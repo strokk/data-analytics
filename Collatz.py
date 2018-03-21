cc2b1f74b2d2bec5f28a68dd400a60fb48631017
strokk <35735075+strokk@users.noreply.github.com>
2018-03-21 18:35:28 +0000

    Update Collatz.py
    added some more comments

--- Collatz.py
+++ Collatz.py
@@ -1,13 +1,16 @@
-#Guilherme Paes, Feb-2018
-#The Collatz conjecture exercise
+# Guilherme Paes, Feb-2018
+# The Collatz conjecture exercise
+# https://en.wikipedia.org/wiki/Collatz_conjecture
 
 n = 21
 
 print(n)
 while n > 1:
   if n % 2 == 0:
+    # if number is even divide by 2
     n = n / 2
     print(n)
   else:
+    # if number is odd multiply by 3 and subtract 1
     n = (n*3) + 1
     print(n)
